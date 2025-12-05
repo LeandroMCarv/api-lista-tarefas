c23b1554e40cb7d4384f2a7e89cd9489332060c6
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-12-05 13:04:24 -0300

    feat: funcionalidade de criar tarefa via API implementada

---
+++ ApiTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,40 @@
+﻿using ApiTarefas.Data;
+using ApiTarefas.Tarefas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TarefaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(Tarefa tarefa)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Tarefas.Add(tarefa);
+                    await _context.SaveChangesAsync();//Por usar o método assincrono ele espera salvar para prosseguir
+
+                    return Created();
+                }
+                else
+                {
+                    return BadRequest("Dados incorretos!");
+                }
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+    }
+}
